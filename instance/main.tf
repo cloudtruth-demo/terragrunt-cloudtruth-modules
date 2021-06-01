03cb859d56ecbc55599a904ae36927fa263b6da2
Darryl Diosomito <69114662+dio-ct@users.noreply.github.com>
2021-06-01 13:33:20 -0400

    Update main.tf

--- instance/main.tf
+++ instance/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.27"
+#    }
+#  }
+#}
 
 
 
